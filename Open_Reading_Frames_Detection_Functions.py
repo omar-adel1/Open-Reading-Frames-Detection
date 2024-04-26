690b2079dcc44e0f3c37bb709536e576dd4a8453
omar-adel1 <omaradelhassan12@gmail.com>
2024-04-26 21:53:28 +0300

    Update find_orfs function documentation
    - Add docstring for function describing arguments and return value
    - Slightly reformat existing code for better readability

--- Open_Reading_Frames_Detection_Functions.py
+++ Open_Reading_Frames_Detection_Functions.py
@@ -1,16 +1,29 @@
 import re
 
 def find_orfs(dna_sequence, min_length):
+    """
+    Finds Open Reading Frames (ORFs) in a DNA sequence.
+
+    Args:
+        dna_sequence (str): The DNA sequence.
+        min_length (int): The minimum length of ORFs to consider.
+
+    Returns:
+        list: A list of tuples containing ORFs along with their start and end positions.
+              Each tuple format: (orf_sequence, start_position, end_position).
+    """
     orfs = []
     if "ATG" in dna_sequence:
+        # Iterate over each occurrence of 'ATG' in the sequence
         for startmatch in re.finditer('ATG', dna_sequence):
-            start_pos = startmatch.start()
-            remaining = dna_sequence[start_pos:]
+            start_pos = startmatch.start() # Get the start position of the ORF
+            remaining = dna_sequence[start_pos:] # Get the remaining sequence starting from the 'ATG'
+            # Iterate over each occurrence of stop codons ('TAA', 'TGA', 'TAG') in the remaining sequence
             for stopmatch in re.finditer('TAA|TGA|TAG', remaining):
-                stop_pos = start_pos + stopmatch.end() - 1
-                orf_sequence = remaining[:stopmatch.end()]
+                stop_pos = start_pos + stopmatch.end() - 1 # Calculate the end position of the ORF
+                orf_sequence = remaining[:stopmatch.end()] # Extract the ORF sequence
                 if len(orf_sequence) >= min_length:
-                    orfs.append((orf_sequence, start_pos, stop_pos))
+                    orfs.append((orf_sequence, start_pos, stop_pos))  # Append ORF details to the list
                 break
     return orfs
 
